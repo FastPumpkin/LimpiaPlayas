126be6ca1e8fb1f3d0cf5154c862695a70762f7a
FastPumpkin <69366346+FastPumpkin@users.noreply.github.com>
2020-10-13 23:14:39 -0500

    Add files via upload

---
+++ src/vision/src/DeteccionDeColor.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+#Autor: Jose Roberto Lopez Valeriano
+
+import rospy
+import cv2 as cv
+import numpy as np 
+from cv_bridge import CvBridge,CvBridgeError
+from sensor_msgs.msg import Image
+from std_msgs.msg import Int32
+
+# Se crean los Publicadores de las coordenadas del centroide del objeto a detectar
+pubx = rospy.Publisher('coordenadas_x',Int32,queue_size=10)
+puby = rospy.Publisher('coordenadas_y',Int32,queue_size=10)
+
+
+
+
+def nothing(x):
+    pass
+
+def showImage(img):
+    #Se definen los TrackBars que definen los limites de los parametros para el espacio de color HSV
+    cv.namedWindow('image')
+    cv.createTrackbar('Hue Minimo', 'image',0,255,nothing)
+    cv.createTrackbar('Hue Maximo', 'image',0,255,nothing)
+    cv.createTrackbar('Saturation Minimo', 'image',0,255,nothing)
+    cv.createTrackbar('Saturation Maximo', 'image',0,255,nothing)
+    cv.createTrackbar('Value Minimo', 'image',0,255,nothing)
+    cv.createTrackbar('Value Maximo', 'image',0,255,nothing)
+    
+
+    hMin = cv.getTrackbarPos('Hue Minimo','image')
+    hMax = cv.getTrackbarPos('Hue Maximo','image')
+    sMin = cv.getTrackbarPos('Saturation Minimo','image')
+    sMax = cv.getTrackbarPos('Saturation Maximo','image')
+    vMin = cv.getTrackbarPos('Value Minimo','image')
+    vMax = cv.getTrackbarPos('Value Maximo','image')
+        
+    lower = np.array([hMin,sMin,vMin])
+    upper = np.array([hMax,sMax,vMax])
+   
+    
+    #Convertir img de RGB -> HSV
+    hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+
+    #Mascara con pixeles dentro del rango de colores establecido
+    mask = cv.inRange(hsv,lower,upper)
+    kernel = np.ones((10,10),np.uint8)
+    mask = cv.morphologyEx(mask,cv.MORPH_OPEN,kernel)
+    mask = cv.morphologyEx(mask,cv.MORPH_CLOSE,kernel)
+
+    #Deteccion de areas dentro de la mascara
+    moments = cv.moments(mask)
+    area = moments['m00']
+    edged = cv.Canny(mask,35,125)
+    image,cnt,hierarchy= cv.findContours(edged,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+  
+
+    if(area > 20000):
+        #Buscar centro x, y del objeto
+        x = int(moments['m10']/moments['m00'])
+        y = int(moments['m01']/moments['m00'])
+
+
+        #publicar e imprimir coordenadas
+        pubx.publish(x)
+        puby.publish(y)
+        
+        #Dibujar marca en el centro del objeto
+        cv.rectangle(img,(x,y),(x+2,y+2),(0,0,255),2)
+    
+    cv.imshow('mask',mask)
+    cv.imshow('image',img)
+    #cv.imshow('edged',edged)
+    cv.waitKey(1)
+
+    
+
+def callback(msg):
+    try:
+        #Se convierte la imagen adquirida en un formato soportado por OpenCv
+        bridge = CvBridge()
+        orig = bridge.imgmsg_to_cv2(msg,"bgr8")
+            
+        showImage(orig)
+
+    except CvBridgeError as e:
+        print(e)
+
+
+rospy.init_node('camera_subs')
+sub = rospy.Subscriber('rgb/image', Image, callback)
+rospy.spin()
---
+++ src/vision/src/NodoCamara.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+#Autor: Jose Roberto Lopez Valeriano
+
+import rospy
+import cv2 as cv
+import numpy as np
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+
+# Se define el dispositivo aa utilizar
+cap = cv.VideoCapture(1)
+
+#La funcion obtiene la imagen proveniente de la camara
+def get_Image():
+    ret,img = cap.read()
+    array = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    return img
+
+#Configuracion de nodo y publicador
+rospy.init_node('camara')
+pub = rospy.Publisher('rgb/image',Image,queue_size=100)
+bridge = CvBridge()
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+    try:
+
+        frame = get_Image()
+        cv.imshow('frame',frame)
+        cv.waitKey(1)
+        frame = np.uint8(frame)
+        #e convierte la imagen a un formato soportado por ROS
+        image_msg = bridge.cv2_to_imgmsg(frame,encoding="bgr8")
+        pub.publish(image_msg)
+        rate.sleep()
+        
+    except TypeError:
+        continue
